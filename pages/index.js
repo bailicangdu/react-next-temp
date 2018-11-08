4ef369ad3e463e2d4795a0a3e3da25a24b80b14d
maguohua1 <1264889788@qq.com>
2018-11-08 19:15:59 +0800

    test deploy

--- pages/index.js
+++ pages/index.js
@@ -2,5 +2,5 @@ import cowsay from 'cowsay-browser'
 
 export default () =>
   <pre>
-    {cowsay.say({ text: 'hi there1111222222!' })}
+    {cowsay.say({ text: 'hi there1111222222333333!' })}
   </pre>
