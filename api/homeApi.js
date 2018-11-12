5a9551c614c3a68e0d393f8ec4341cef070ece97
maguohua1 <1264889788@qq.com>
2018-11-12 23:26:23 +0800

    test data

--- api/homeApi.js
+++ api/homeApi.js
@@ -1,7 +1,7 @@
 import Axios from './axios.js';
 
 class HomeApi extends Axios {
-  getCity = (params, headers) => this.axios('get', 'http://127.0.0.1:8001/v1/cities', params, headers);
+  getCity = (params, headers) => this.axios('get', 'https://elm.cangdu.org/v1/cities', params, headers);
   testssr = (params, headers) => this.axios('get', 'http://localhost:1234/testssr', params, headers);
 }
 
